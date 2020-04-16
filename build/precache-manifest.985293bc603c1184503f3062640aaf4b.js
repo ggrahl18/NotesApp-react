e3a8467236205860573c95350044876e8861ae02
boss <ggrahl18@gmail.com>
2020-04-15 21:48:39 -0500

    build added, index changed

---
+++ build/precache-manifest.985293bc603c1184503f3062640aaf4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb44c15648a4908b610a0c05756a9f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f1e0be110fdac628c8b",
+    "url": "/static/css/main.4fdb3639.chunk.css"
+  },
+  {
+    "revision": "6c05b165a2e2c653f440",
+    "url": "/static/js/2.dfcb2cf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dfcb2cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1e0be110fdac628c8b",
+    "url": "/static/js/main.1774d83f.chunk.js"
+  },
+  {
+    "revision": "5407a9df65edc59158cb",
+    "url": "/static/js/runtime-main.868a790d.js"
+  }
+]);
