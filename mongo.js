fd5044c54d500ee73ef8ce689a1c1f7944f38d86
boss <ggrahl18@gmail.com>
2020-04-19 14:25:44 -0500

    uibuild

--- mongo.js
+++ mongo.js
@@ -1,39 +1,41 @@
 const mongoose = require('mongoose')
 
-if ( process.argv.length<3 ) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
-const password = process.argv[2]
-
-const url =
-  `mongodb+srv://bossMan:${password}@cluster0-1mffo.mongodb.net/note-app?retryWrites=true&w=majority`
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: 'I hope this prepares me for b9labs',
-  date: new Date(),
-  important: true,
-})
-
-note.save().then(response => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
-
-// Note.find({}).then(result => {
-//     result.forEach(note => {
-//       console.log(note)
-//     })
-//     mongoose.connection.close()
-//   })
+  if ( process.argv.length<3 ) {
+    console.log('give password as argument')
+    process.exit(1)
+  }
+  
+  const password = process.argv[2]
+  
+  const url =
+    `mongodb+srv://bossMan:${password}@cluster0-1mffo.mongodb.net/note-app?retryWrites=true&w=majority`
+  
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean,
+  })
+  
+  const Note = mongoose.model('Note', noteSchema)
+  
+  const note = new Note({
+    content: 'Callback-functions suck',
+    date: new Date(),
+    important: true,
+  })
+  
+  /*
+  note.save().then(response => {
+    console.log('note saved!');
+    mongoose.connection.close();
+  })
+  */
+  
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
